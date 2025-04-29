40e3831d6c89e045b31f30e02dc849c669dd7320
s2301089 <s2301089@sendai-nct.jp>
2025-04-29 15:35:35 +0900

    first commit

--- codes/arduino_controller/DUALSENSE/ArduinoTransmitForDualSense/src/main.cpp
+++ codes/arduino_controller/DUALSENSE/ArduinoTransmitForDualSense/src/main.cpp
@@ -1,18 +1,13 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(13,OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  digitalWrite(13,1);
+  delay(250);
+  
+  digitalWrite(13,0);
+  delay(250);
 }
