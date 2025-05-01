fa1c3cef18548f03c4525e39fb65d7308adb1a0d
s2301089 <s2301089@sendai-nct.jp>
2025-05-01 18:56:48 +0900

    add playstationcontroller

---
+++ codes/arduino_controller/PlayStationController/src/getController.h
@@ -0,0 +1,57 @@
+/*
+getController.h
+*/
+
+#ifndef INC_GET_CONTROLLER_H_
+#define INC_GET_CONTROLLER_h_
+
+#include <stdint.h>
+
+enum ControllerType{
+    DUALSHOCK3,
+    DUALSHOCK4,
+    DUALSENSE
+}
+
+typedef struct{
+    // analog values 0~255
+    uint8_t LX;
+    uint8_t LY;
+    uint8_t L2;
+    uint8_t RX;
+    uint8_t RY;
+    uint8_t R2;
+
+    // button status 0,1
+    bool TRIANGLE;
+    bool CIRCLE;
+    bool CROSS;
+    bool SQUARE;
+    bool UP;
+    bool RIGHT;
+    bool DOWN;
+    bool LEFT;
+    bool L1;
+    bool L3;
+    bool R1;
+    bool R3;
+    bool PS;
+
+    // DUALSHOCK3
+    bool SELECT;
+    bool START;
+
+    // DUALSHOCK4
+    bool SHARE;
+
+    // DUALSENSE
+    bool CREATE;
+    bool MICROPHONE; // 送信データ割当場所不足により不使用中
+
+    // DUALSHOCK4 & DUALSENSE
+    bool OPTIONS;
+    bool TOUCHPAD;
+} Data;
+
+
+#endif /* INC_GET_CONTROLLER_H_ */
---
+++ codes/arduino_controller/PlayStationController/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+bool getController(char *type);
+
+void setup() {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+bool getController(char *type){
+
+}
