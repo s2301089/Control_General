9f88bbf5793d23adf575dbe8ce4b00ffa5bb7294
s2301089 <s2301089@sendai-nct.jp>
2025-05-10 09:40:12 +0900

    successed check controller types

--- codes/arduino_controller/PlayStationController/src/main.cpp
+++ codes/arduino_controller/PlayStationController/src/main.cpp
@@ -4,19 +4,24 @@
 SoftwareSerial outSerial(7,6);
 Data ControllerData;
 
+USB bsu;
+PS3USB ps3(&bsu);
+PS4USB ps4(&bsu);
+
 void setup() {
   Serial.begin(38400);
   outSerial.begin(38400);
+  bsu.Init();
 }
 
 void loop() {
-  bool status;
-  status = getController(ControllerType::DUALSENSE,&ControllerData);
-  if(status == SUCCESS_CODE){
-    transmitController(&outSerial,ControllerData);
-    showControllerData(&Serial,ControllerData);
-}else{
-    Serial.println(" getController was failed");
+  bsu.Task();
+  if(ps3.PS3Connected || ps3.PS3NavigationConnected){
+    Serial.println("PS3 Controller is Connected.");
+  }else if(ps4.connected()){
+    Serial.println("PS4 Controller is Connected.");
+  }else {
+    Serial.println("Controller is not Connected.");
   }
 }
 
