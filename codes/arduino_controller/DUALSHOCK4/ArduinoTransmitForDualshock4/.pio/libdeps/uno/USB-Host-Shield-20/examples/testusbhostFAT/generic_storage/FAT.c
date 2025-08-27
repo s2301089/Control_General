82bff3c9e0b182016bf6a0af29687d807aab808d
s2301089 <190147344+s2301089@users.noreply.github.com>
2025-08-27 10:15:47 +0900

    rm codes dir;

--- codes/arduino_controller/DUALSHOCK4/ArduinoTransmitForDualshock4/.pio/libdeps/uno/USB-Host-Shield-20/examples/testusbhostFAT/generic_storage/FAT.c
+++
@@ -1,4 +0,0 @@
-#include <Arduino.h>
-#include <FAT/FatFS/src/option/unicode.c>
-#include <FAT/FatFS/src/option/syscall.c>
-#include <FAT/FatFS/src/ff.c>
--- codes/arduino_controller/DUALSHOCK4/ArduinoTransmitForDualshock4/.pio/libdeps/uno/USB-Host-Shield-20/examples/testusbhostFAT/generic_storage/generic_storage.cpp
+++
@@ -1,3 +0,0 @@
-/* Yup, just a bunch of includes. */
-#include "PCpartition/PCPartition.cpp"
-#include"FAT/FAT.cpp"
